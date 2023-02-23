5ad002240fb08eae0c3d6184e541a32d22dd7fbf
Sachin Yo <sachin_aryal>
2023-02-23 10:09:37 +0545

    Added mantine and additional eslint and prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+/**
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: false,
+  endOfLine: "auto",
+  trailingComma: "es5",
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import '~/styles/globals.css'
 
-import { api } from "~/utils/api";
+import { MantineProvider } from '@mantine/core'
+import { type AppType } from 'next/app'
+import { type Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
 
-import "~/styles/globals.css";
+import { api } from '~/utils/api'
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,9 +13,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Component {...pageProps} />
+      </MantineProvider>
     </SessionProvider>
-  );
-};
+  )
+}
 
-export default api.withTRPC(MyApp);
+export default api.withTRPC(MyApp)
